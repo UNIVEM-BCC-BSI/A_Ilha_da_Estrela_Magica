58a0c1819b9022c6070def60686301077f69da19
Arthur_Hoshino <99358471+ArthurHoshino@users.noreply.github.com>
2023-04-18 19:20:06 -0400

    Add files via upload

---
+++ Level.py
@@ -0,0 +1,82 @@
+# ENEMY SELECTION
+import pygame
+import perguntas
+
+perguntas = perguntas.perguntas
+
+class LevelState():
+    def __init__(self, group_btn: list, option_btn: list, screen):
+        self.level_state = 'level_1' # which level the user is
+        self.group_btn = group_btn
+        self.option_btn = option_btn
+        self.screen = screen
+
+        self.state = ''
+
+    def level_1(self):
+        # Enemy 1 button
+        self.group_btn[0].update()
+        self.group_btn[0].draw(self.screen)
+
+        # Enemy 2 button disabled
+        self.group_btn[7].update()
+        self.group_btn[7].draw(self.screen)
+
+
+        # Enemy 3 button disabled
+        self.group_btn[8].update()
+        self.group_btn[8].draw(self.screen)
+
+        # Option enemy 1 button
+        if self.option_btn[0].mouse_click() == True:
+            self.state = 'battle'
+            self.option_btn[0].reset_state()
+    
+    def level_2(self):
+        # level 1 disabled
+        self.group_btn[3].update()
+        self.group_btn[3].draw(self.screen)
+
+        # level 2
+        self.group_btn[1].update()
+        self.group_btn[1].draw(self.screen)
+
+        # Enemy 3 button disabled
+        self.group_btn[8].update()
+        self.group_btn[8].draw(self.screen)
+
+        if self.option_btn[1].mouse_click() == True:
+            self.state = 'battle'
+            self.option_btn[1].reset_state()
+        
+    def level_3(self):
+        # level 1 disabled
+        self.group_btn[3].update()
+        self.group_btn[3].draw(self.screen)
+
+        # level 2 disabled
+        self.group_btn[4].update()
+        self.group_btn[4].draw(self.screen)
+    
+        # level 3
+        self.group_btn[2].update()
+        self.group_btn[2].draw(self.screen)
+
+        if self.option_btn[2].mouse_click() == True:
+            self.state = 'battle'
+            self.option_btn[2].reset_state()
+
+    def level_manager(self):
+        if (self.level_state == 'level_1'):
+            self.level_1()
+        
+        if (self.level_state == 'level_2'):
+            self.level_2()
+        
+        if (self.level_state == 'level_3'):
+            self.level_3()
+        
+        return self.state
+
+    def reset_state(self):
+        self.state = ''
